79904c37778b7bdd29349d2fd7d01b12e516f90e
Aurora <mengxia.ge@outlook.com>
2023-03-16 18:16:44 +0800

    master commit 1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello, git2");
         System.out.println("hello, git3");
         System.out.println("hello, git4 by hot-fix");
+        System.out.println("hello, git5 by master");
 
     }
 }
