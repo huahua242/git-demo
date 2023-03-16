62be77162f8eebcb1f6548309493916909a2af3f
Aurora <mengxia.ge@outlook.com>
2023-03-16 17:59:48 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: mengx
+ * @Date: 2023/03/16/17:56
+ * @Description:
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git");
+    }
+}
---
+++ src/main/java/GitTest2.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: mengx
+ * @Date: 2023/03/16/17:57
+ * @Description:
+ * @Version: 1.0
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("hi, git");
+    }
+}
