5228ba38464d8f9752be08918cf1e75833cae67b
huahua242 <100401595+huahua242@users.noreply.github.com>
2023-03-16 18:49:13 +0800

    pull test

--- src/main/java/GitTest2.java
+++ src/main/java/GitTest2.java
@@ -8,5 +8,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hi, git");
         System.out.println("master push 1");
+        System.out.println("pull test");
     }
 }
